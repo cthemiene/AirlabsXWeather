84721d633db8ced5814d8e05dd609c2c8c038bda
Camron Miene <55463492+cthemiene@users.noreply.github.com>
2023-01-03 20:53:40 -0600

    Add files via upload

---
+++ Weather_import_requests.py
@@ -0,0 +1,19 @@
+import requests
+import json
+
+# Set the API key
+api_key = "TGEAxQmxbqSEIeobYDfCrIRiFApdnaxF"
+
+# Set the station ID
+station_id = "GHCND:USW00014739"
+
+# Make the request to the NOAA web service
+# Instead of staion use either ATL lat and long or active flight lat and long
+# https://api.weather.gov/points/{latitude},{longitude}
+response = requests.get(f"https://www.ncdc.noaa.gov/cdo-web/api/v2/stations/{station_id}", headers={'token': api_key})
+
+# Parse the JSON response
+Weather_data = response.json()
+
+# Print the response
+print(Weather_data)
